f63b63fa4a8c6b997db60a563b6b0a8b04428fd5
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2016-02-20 18:56:21 +0500

    Add PIL and sphinx_rtd_theme to configs

--- source/conf.py
+++ source/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import shlex
+import sphinx_rtd_theme
 from recommonmark.parser import CommonMarkParser
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -126,7 +127,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
