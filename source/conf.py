473d2cbe0f0554d027e0e467fb4fdf1bb227a2cb
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2018-01-12 19:10:40 +0500

    Fix bug in new Sphinx version

--- source/conf.py
+++ source/conf.py
@@ -18,6 +18,10 @@ import shlex
 import sphinx_rtd_theme
 from recommonmark.parser import CommonMarkParser
 
+from docutils.parsers.rst.directives.admonitions import BaseAdmonition
+from sphinx.util import compat
+compat.make_admonition = BaseAdmonition
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
